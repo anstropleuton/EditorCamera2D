5540d22ac55289827d9fef67eb703f47cf6da5db
Anstro Pleuton <anstro.pleuton@gmail.com>
2026-06-14 13:40:39 +0530

    Fix style and naming to me more aligned with raylib

--- main.c
+++ main.c
@@ -3,41 +3,45 @@
 
 typedef struct EditorCamera2D {
     Camera2D camera;
-    float    zoom_speed;
-    float    min_zoom;
-    float    max_zoom;
+    float zoomSpeed;
+    float minZoom;
+    float maxZoom;
 } EditorCamera2D;
 
-void UpdateEditorCamera(EditorCamera2D *camera) {
+void UpdateEditorCamera(EditorCamera2D* camera)
+{
     if (!camera) return;
 
     Vector2 dpi = GetWindowScaleDPI();
 
-    camera->camera.offset = (Vector2){(float) GetRenderWidth() / 2.0F, (float) GetRenderHeight() / 2.0F};
+    camera->camera.offset = (Vector2){(float)GetRenderWidth() / 2.0F, (float)GetRenderHeight() / 2.0F};
 
-    if (IsMouseButtonDown(MOUSE_BUTTON_MIDDLE)) {
+    if (IsMouseButtonDown(MOUSE_BUTTON_MIDDLE))
+    {
         // target -= rotate(mouse delta, -rotation) / zoom
         camera->camera.target = Vector2Subtract(camera->camera.target, Vector2Scale(Vector2Rotate(GetMouseDelta(), -camera->camera.rotation * DEG2RAD), 1.0F / camera->camera.zoom));
     }
 
-    if (GetMouseWheelMove() != 0) {
-        float nextZoom = exp2f(log2f(camera->camera.zoom) + camera->zoom_speed * GetMouseWheelMove());
+    if (GetMouseWheelMove() != 0)
+    {
+        float nextZoom = exp2f(log2f(camera->camera.zoom) + camera->zoomSpeed * GetMouseWheelMove());
         // target += rotate(mouse position * dpi, -rotation) * (1 / zoom - 1 / next zoom)
         camera->camera.target = Vector2Add(camera->camera.target, Vector2Scale(Vector2Rotate(Vector2Subtract(Vector2Multiply(GetMousePosition(), dpi), camera->camera.offset), -camera->camera.rotation * DEG2RAD), 1.0F / camera->camera.zoom - 1.0F / nextZoom));
-        camera->camera.zoom   = nextZoom;
+        camera->camera.zoom = nextZoom;
     }
 }
 
-int main(void) {
+int main(void)
+{
     SetConfigFlags(FLAG_WINDOW_HIGHDPI | FLAG_WINDOW_RESIZABLE);
     InitWindow(1200, 600, "Editor camera 2D");
     SetTargetFPS(144);
 
     EditorCamera2D camera = {0};
     camera.camera.zoom = 0.1F;
-    camera.zoom_speed  = 0.1F;
-    camera.min_zoom    = 0.015625F;
-    camera.max_zoom    = 64.0F;
+    camera.zoomSpeed = 0.1F;
+    camera.minZoom = 0.015625F;
+    camera.maxZoom = 64.0F;
 
     Texture texture[] = {
         LoadTexture("tile_00.png"),
@@ -48,9 +52,10 @@ int main(void) {
 
     int size = sizeof(texture) / sizeof(texture[0]);
 
-    int text_size = (int)(20.0F / fminf(GetWindowScaleDPI().x, GetWindowScaleDPI().y));
+    int textSize = (int)(20.0F / fminf(GetWindowScaleDPI().x, GetWindowScaleDPI().y));
 
-    while (!WindowShouldClose()) {
+    while (!WindowShouldClose())
+    {
         UpdateEditorCamera(&camera);
 
         BeginDrawing();
@@ -58,19 +63,21 @@ int main(void) {
 
         BeginMode2D(camera.camera);
 
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++)
+        {
             DrawTexture(texture[i], 8192 * i, 0, WHITE);
         }
 
         EndMode2D();
 
-        DrawText("Panorama di Manerba del Garda con Pieve Rocca Montinelle Solarolo", 20, GetScreenHeight() - 60, text_size, BLACK);
-        DrawText("Photo by Wolfgang Moroder, CC BY-SA 4.0", 20, GetScreenHeight() - 40, text_size, BLACK);
+        DrawText("Panorama di Manerba del Garda con Pieve Rocca Montinelle Solarolo", 20, GetScreenHeight() - 60, textSize, BLACK);
+        DrawText("Photo by Wolfgang Moroder, CC BY-SA 4.0", 20, GetScreenHeight() - 40, textSize, BLACK);
 
         EndDrawing();
     }
 
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         UnloadTexture(texture[i]);
     }
 
